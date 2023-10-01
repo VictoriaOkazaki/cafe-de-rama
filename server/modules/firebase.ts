953b682b681c4630646be7a5fb317d1446d1a8d4
VictoriaOkazaki <victoria.timein@gmail.com>
2023-10-01 17:42:48 +0400

    1.0.7

--- server/modules/firebase.ts
+++ server/modules/firebase.ts
@@ -7,11 +7,14 @@ export function initFirebaseApp() {
   console.log('process.env.FIREBASE_PROJECT_ID', process.env.FIREBASE_PROJECT_ID)
   console.log('process.env.FIREBASE_CLIENT_EMAIL', process.env.FIREBASE_CLIENT_EMAIL)
   console.log('process.env.FIREBASE_PRIVATE_KEY', process.env.FIREBASE_PRIVATE_KEY)
+
+  const privateKey = (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n')
+
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      privateKey,
     }),
     storageBucket: "gs://cafe-de-rama-48f0c.appspot.com",
   });
