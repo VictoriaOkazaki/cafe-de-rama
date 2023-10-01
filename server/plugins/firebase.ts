25837c33461cd2c7e1c8cce4f7bfdb174dd6ed63
VictoriaOkazaki <victoria.timein@gmail.com>
2023-10-01 16:40:29 +0400

    1.0.2

--- server/plugins/firebase.ts
+++ server/plugins/firebase.ts
@@ -1,6 +1,9 @@
 import admin from "firebase-admin";
 
 export default defineNitroPlugin(() => {
+  console.log('process.env.FIREBASE_PROJECT_ID', process.env.FIREBASE_PROJECT_ID)
+  console.log('process.env.FIREBASE_CLIENT_EMAIL', process.env.FIREBASE_CLIENT_EMAIL)
+  console.log('process.env.FIREBASE_PRIVATE_KEY', process.env.FIREBASE_PRIVATE_KEY)
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
